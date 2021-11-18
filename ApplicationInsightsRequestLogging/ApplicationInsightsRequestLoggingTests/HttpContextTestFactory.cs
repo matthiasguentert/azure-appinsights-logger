e2927fdb35f7190c7fd2e619a21ff21c8c1e85b6
Matthias Güntert <matthias.guentert@gmail.com>
2021-11-18 16:08:03 +0100

    Added new tests

--- ApplicationInsightsRequestLogging/ApplicationInsightsRequestLoggingTests/HttpContextTestFactory.cs
+++ ApplicationInsightsRequestLogging/ApplicationInsightsRequestLoggingTests/HttpContextTestFactory.cs
@@ -14,10 +14,33 @@ namespace ApplicationInsightsRequestLoggingTests
 
             streamWriter.WriteLine(body);
             streamWriter.Flush();
-            memoryStream.Position = 0;
+            
+            memoryStream.Seek(0, SeekOrigin.Begin);
 
             context.Request.Body = memoryStream;
             context.Request.ContentLength = body.Length;
+
+            return context;
+        }
+
+        public static DefaultHttpContext CreateWithResponseBody(string body)
+        {
+            var context = new DefaultHttpContext();
+
+            var memoryStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memoryStream);
+
+            streamWriter.WriteLine(body);
+            streamWriter.Flush();
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            context.Response.Body = memoryStream;
+            context.Response.ContentLength = body.Length;
+
+            var r = new StreamReader(memoryStream);            
+            var foo = r.ReadToEnd();
+            memoryStream.Seek(0, SeekOrigin.Begin);
 
             return context;
         }
